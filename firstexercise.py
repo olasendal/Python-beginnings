18c7765eebe74afb0af5bda7e54352762276386e
olasendal <43734526+olasendal@users.noreply.github.com>
2019-01-18 14:40:28 +1300

    Add files via upload

---
+++ firstexercise.py
@@ -0,0 +1,8 @@
+#that's the first exercise for calculating how much calories I should get daily#
+weight = 50
+height = 155
+age = 31
+s = -161
+PPM = 10 * weight + 6.25 * height + 5 * age + s
+print ("I should get", PPM * 1.6, "calories daily")
+
